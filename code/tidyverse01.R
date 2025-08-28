29429ddd6d68bfbf9876795f0b3621c510105b2f
zolzaya813 <z_amarsanaa@uncg.edu>
2025-08-28 16:03:42 -0400

    basic

---
+++ code/tidyverse01.R
@@ -0,0 +1,11 @@
+if(!require(pacman)) install.packages("pacman")
+pacman::p_load(tidyverse)
+
+set.seed(123)
+
+iris_sub <- as_tibble(iris) %>% 
+  group_by(Species) %>% 
+  sample_n(3) %>% 
+  ungroup()
+
+print(iris_sub)
