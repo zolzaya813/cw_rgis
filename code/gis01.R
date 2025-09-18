c7329119660f7068ee6a5022e66f450d7f5abac8
zolzaya813 <z_amarsanaa@uncg.edu>
2025-09-18 15:07:44 -0400

    in class

---
+++ code/gis01.R
@@ -0,0 +1,53 @@
+if (!require(pacman)) install.packages("pacman")
+
+pacman::p_load(tidyverse,
+               sf,
+               mapview)
+
+df_fish <- read_csv("data/data_finsync_nc.csv")
+print(df_fish)
+
+sf_site <- df_fish %>% 
+  distinct(site_id, lon, lat) %>% # get unique combinations of longitude & latitude
+  st_as_sf(coords = c("lon", "lat"),
+           crs = 4326)
+
+print(sf_site)
+
+# mapview(sf_site,
+#         legend = FALSE)
+
+##export data
+saveRDS(sf_site,
+        file = "data/sf_finsync_nc.rds")
+
+# conversion from geodetic to projected -----------------------------------
+
+sf_ft_wgs <- sf_site %>% 
+  slice(c(1, 2)) # slice selects by the rows
+
+print(sf_ft_wgs)
+
+sf_ft_utm <- sf_ft_wgs %>% 
+  st_transform(crs = 32617)
+
+print(sf_ft_utm)
+
+st_distance(sf_ft_utm)
+
+
+# exercise ----------------------------------------------------------------
+
+df_quakes <- as.tibble(quakes)
+print(df_quakes)
+
+sf_quakes <- df_quakes %>% 
+  st_as_sf(coords = c("long", "lat"),
+           crs = 4326)
+sf_ft_quakes <- sf_quakes %>% 
+  slice(c(1,2))
+
+sf_ft_quakes_proj <- sf_ft_quakes %>% 
+  st_transform(crs = 32617)
+
+saveRDS(sf_quakes, file = "data/sf_quakes.rds")
